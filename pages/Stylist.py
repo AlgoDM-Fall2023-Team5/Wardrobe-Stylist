0f57a72132dfa72b6332d07e76d3136a78109df4
Shirish <shirishaddaganti@gmail.com>
2023-12-12 17:07:43 -0500

    done error handling

--- pages/Stylist.py
+++ pages/Stylist.py
@@ -11,30 +11,39 @@ import boto3
 from snowflake_list import annotations_list
 from macys_items import fetch_product_info
 
-
 # Load CLIP model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
+try:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model= clip.load("ViT-B/32", device=device)[0]
+except Exception as e:
+    st.error(f"Error loading CLIP model: {str(e)}")
 
 # Collect AWS secrets
-bucket_name = st.secrets.aws_credentials.bucket_name
-service_name = st.secrets.aws_credentials.service_name
-aws_access_key_id = st.secrets.aws_credentials.aws_access_key_id
-aws_secret_access_key = st.secrets.aws_credentials.aws_secret_access_key
-region_name = st.secrets.aws_credentials.region_name
+try:
+    bucket_name = st.secrets.aws_credentials.bucket_name
+    service_name = st.secrets.aws_credentials.service_name
+    aws_access_key_id = st.secrets.aws_credentials.aws_access_key_id
+    aws_secret_access_key = st.secrets.aws_credentials.aws_secret_access_key
+    region_name = st.secrets.aws_credentials.region_name
 
-# S3 client setup
-s3_client = boto3.client(
-    service_name=service_name,
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=region_name
-)
+    # S3 client setup
+    s3_client = boto3.client(
+        service_name=service_name,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
+except Exception as e:
+    st.error(f"Error collecting AWS secrets: {str(e)}")
 
 snowflake_url = st.secrets.project_snowflake.url
 
 # role ######################
-wardrobe_list = annotations_list(snowflake_url)
+try:
+    wardrobe_list = annotations_list(snowflake_url)
+except Exception as e:
+    st.error(f"Error fetching wardrobe list: {str(e)}")
+
 role = """
 you are my personal wardrobe assistant who has immense experience in the fashion industry. You know the latest trends and people appreciate you often for your fashion choices. You are also funny and give the best advice based on the event. Now, these are my wardrobe data:
 Now, answer only the questions I ask and suggest only from the wardrobe.\n {wardrobe_list}
@@ -42,15 +51,12 @@ Return in the following format:\n.
 "Top : (color: ,clothing type: ,pattern:), 
 Bottom : (color: ,clothing type:,pattern:) 
 Reason:"\n 
-Please only give me responses in the specified format.No spelling error in keys. Remember, the choice should match the exact keyword from the wardrobe. Also, try to include a funny thing in the reason based on the occasion. Return the output in json only.
+Please only give me responses in the specified format. No spelling error in keys. Remember, the choice should match the exact keyword from the wardrobe. Also, try to include a funny thing in the reason based on the occasion. Return the output in json only.
 If any questions other than fashion are asked kindly reply in your words you are not able to. 
 """.format(
     wardrobe_list="\n".join(wardrobe_list)
 )
 Gender = "Men"
-
-
-
 
 # Load image features and image IDs from S3
 @st.cache_data
@@ -82,7 +88,6 @@ def display_images_from_s3(image_ids):
         image = Image.open(BytesIO(image_data))
         columns[j].image(image, caption=f"Image {j+1}")
 
-
 # Function to encode search query
 @st.cache_data
 def encode_search_query(search_query):
@@ -92,12 +97,10 @@ def encode_search_query(search_query):
     return text_encoded
 
 # Function to find best matches
-
 def find_best_matches(text_features, image_features, image_ids, results_count=3):
     similarities = (image_features @ text_features.T).squeeze(1)
     best_image_idx = (-similarities).argsort()
     return [image_ids[i] for i in best_image_idx[:results_count]]
-
 
 # Function for image search
 @st.cache_data()
@@ -105,7 +108,6 @@ def search(search_query, results_count=3):
     image_features, image_ids = load_features_ids()
     text_features = encode_search_query(search_query)
     return find_best_matches(text_features, image_features, image_ids, results_count)
-
 
 def main():
     st.sidebar.title("Chat")
@@ -127,81 +129,90 @@ def interact_with_gpt(question, key, role=role):
       The response from GPT-3.5.
     """
 
-    api_key = key
-    client = OpenAI(api_key=api_key)
+    try:
+        api_key = key
+        client = OpenAI(api_key=api_key)
 
-    # Define the parameters for the request
-    messages = [
-        {"role": "system", "content": role},
-        {"role": "user", "content": question},
-    ]
-    temperature = 0.3
+        # Define the parameters for the request
+        messages = [
+            {"role": "system", "content": role},
+            {"role": "user", "content": question},
+        ]
+        temperature = 0.3
 
-    # Send the request to OpenAI
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo", messages=messages, temperature=temperature
-    )
+        # Send the request to OpenAI
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo", messages=messages, temperature=temperature
+        )
 
-    answer = response.choices[0].message.model_dump_json()
-    # Return the response
-    return answer
+        answer = response.choices[0].message.model_dump_json()
+        # Return the response
+        return answer
+
+    except Exception as e:
+        st.error(f"Error interacting with GPT-3.5: {str(e)}")
+        return None
 
 if __name__ == "__main__":
     st.title("Stylist Opinion")
 
-    # question ###################
-    openai_key = st.sidebar.text_input("Enter your OpenAI Key :")
+    try:
+        # question ###################
+        openai_key = st.sidebar.text_input("Enter your OpenAI Key :")
 
-    question = main()
+        question = main()
 
-    # handling None while display
-    if question is None:
-        st.write(
-            """:red[Please] :orange[provide] :green[details] :blue[about] :violet[your] :gray[outfit.]"""
-        )
+        # handling None while display
+        if question is None:
+            st.write(
+                """:red[Please] :orange[provide] :green[details] :blue[about] :violet[your] :gray[outfit.]"""
+            )
 
-    else:
-        response = interact_with_gpt(question=question, key=openai_key)
-        response_json = json.loads(json.loads(response)['content'])
-
-        st.write("Top Wear")
-        #st.write(response_json['Top'])
-        top_string = json.dumps(response_json['Top'])
-        display_images_from_s3(search(top_string))
-
-        st.write("Bottom Wear")
-        #st.write(response_json['Bottom'])
-        bottom_string = json.dumps(response_json['Bottom'])
-        display_images_from_s3(search(bottom_string))
-
-        st.write("Reason")
-        st.write(response_json['Reason'])
-
-        # Recommendations
-        st.header("Top Recommendations:")
-        top_recommendations = fetch_product_info(response_json['Top']['color'],
-                                                  response_json['Top']['clothing type'],
-                                                  "outerwear",
-                                                  response_json['Top']['pattern'],
-                                                  Gender)
-        if top_recommendations:
-            count = 1
-            for product in top_recommendations:
-                st.write(f"Product {count}: [link]" + "www.macys.com" + f"{product['product_url']}")
-                count = count + 1
         else:
-            st.write("Error Retrieving Recommendations")
+            response = interact_with_gpt(question=question, key=openai_key)
+            if response is not None:
+                response_json = json.loads(json.loads(response)['content'])
 
-        st.header("Bottom Recommendations:")
-        Bottom_recommendations = fetch_product_info(response_json['Bottom']['color'],
-                                                  response_json['Bottom']['clothing type'],
-                                                  response_json['Bottom']['pattern'],
-                                                  Gender)
+                st.write("Top Wear")
+                top_string = json.dumps(response_json['Top'])
+                display_images_from_s3(search(top_string))
+
+                st.write("Bottom Wear")
+                bottom_string = json.dumps(response_json['Bottom'])
+                display_images_from_s3(search(bottom_string))
+
+                st.write("Reason")
+                st.write(response_json['Reason'])
+
+                # Recommendations
+                st.header("Top Recommendations:")
+                top_recommendations = fetch_product_info(response_json['Top']['color'],
+                                                          response_json['Top']['clothing type'],
+                                                          "outerwear",
+                                                          response_json['Top']['pattern'],
+                                                          Gender)
+                if top_recommendations:
+                    count = 1
+                    for product in top_recommendations:
+                        st.write(f"Product {count}: [link]" + "www.macys.com" + f"{product['product_url']}")
+                        count = count + 1
+                else:
+                    st.write("Error Retrieving Recommendations")
+
+                st.header("Bottom Recommendations:")
+                Bottom_recommendations = fetch_product_info(response_json['Bottom']['color'],
+                                                            response_json['Bottom']['clothing type'],
+                                                            response_json['Bottom']['pattern'],
+                                                            Gender)
+
+                if Bottom_recommendations:
+                    count = 1
+                    for product in Bottom_recommendations:
+                        st.write(f"Product {count}: [link]" + "www.macys.com" + f"{product['product_url']}")
+                        count = count + 1
+                else:
+                    st.write("Error Retrieving Recommendations")
+
+    except Exception as e:
+        st.error(f"Please Enter Valid Input or Try Again.An unexpected error occurred: {str(e)}")
         
-        if Bottom_recommendations:
-            count = 1
-            for product in Bottom_recommendations:
-                st.write(f"Product {count}: [link]" + "www.macys.com" + f"{product['product_url']}")
-                count = count + 1
-        else:
-            st.write("Error Retrieving Recommendations")
