068b180cdafc36dbb2c011991e953822addeae2e
saisrinivas-madamraju <145714507+saisrinivas-madamraju@users.noreply.github.com>
2023-12-16 02:59:09 +0000

    video link

--- Main.py
+++ Main.py
@@ -9,9 +9,10 @@ def main():
 
     st.header("👕 Your Style, Your Way")
     st.write("Embarking on a revolutionary journey in fashion technology, our Personal Wardrobe Stylist Application redefines how customers explore and embrace style. Seamlessly blending cutting-edge AI technology with an extensive product inventory, our mission is to simplify the daunting task of selecting the perfect outfit for every occasion. The interface serves as a gateway to this transformative experience, offering an intuitive platform where users effortlessly input event details, initiating a personalized fashion voyage. Through Streamlit's streamlined interface, users engage in a sophisticated yet user-friendly space, triggering AI-powered algorithms to craft bespoke fashion recommendations. This amalgamation of technology promises a refined, convenient, and engaging fashion exploration for our users.")
-    # Video with a stylish frame
-    # st.header("🎥 Feature Highlight")
-    # st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ", format="video/mp4")
+   
+    Video with a stylish frame
+    st.header("🎥 Feature Highlight")
+    st.video("https://youtu.be/TG9C7n1HXBA", format="video/mp4")
 
     # User Guide with a call-to-action button
     st.header("📖 User Guide")
